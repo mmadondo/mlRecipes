f7b3507bd4780c45b3381621e9faf3d850f0dd0e
Malvern <mmadondo@users.noreply.github.com>
2018-01-11 21:01:52 -0600

    Added examples in comments

--- helloFruits.py
+++ helloFruits.py
@@ -1,6 +1,12 @@
 from sklearn import tree
 import matplotlib.pyplot as plt
-#training data to classify fruits: orange vs apple
+#training data to classify fruits: 
+# Examples
+# Weight Texture Label
+# 150g   Bumpy   Orange
+# 170g   Bumpy   Orange
+# 140g   Smooth  Apple
+# 130g   Smooth  Apple
 
 # features: 1 = smooth and 0 = bumpy
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
